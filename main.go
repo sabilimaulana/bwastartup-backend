496716c95572f628e5ec80056d45cb98a5cc1cb7
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-20 19:48:32 +0700

    Gorm koneksi ke database

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main(){
-	
+import (
+	"bwastartup/user"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection to database is success")
+
+	var users []user.User
+
+	db.Debug().Find(&users)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("=============")
+	}
 }
