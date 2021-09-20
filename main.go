fe946406df402e0b5588ce69a943d42a3dd37312
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-20 19:56:05 +0700

    Menghasilkan output JSON

--- main.go
+++ main.go
@@ -2,30 +2,53 @@ package main
 
 import (
 	"bwastartup/user"
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 
+	// dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connection to database is success")
+
+	// var users []user.User
+
+	// db.Debug().Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("=============")
+	// }
+
+	router := gin.Default()
+	router.GET("/users", handler)
+
+	router.Run()
+
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database is success")
-
 	var users []user.User
 
 	db.Debug().Find(&users)
 
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("=============")
-	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": users,
+	})
 }
