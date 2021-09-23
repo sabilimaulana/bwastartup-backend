1c14b1efb065d4569dc1ec1da13bebb29d977812
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-23 07:48:20 +0700

    List Campaign API : JSON response formatter

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,37 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	campaignFormatter := CampaignFormatter{
+		ID:               campaign.ID,
+		Name:             campaign.Name,
+		ShortDescription: campaign.Name,
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+		ImageURL:         "",
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	return campaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	var campaignsFormater []CampaignFormatter
+
+	for _, c := range campaigns {
+		campaignsFormater = append(campaignsFormater, FormatCampaign(c))
+	}
+
+	return campaignsFormater
+}
