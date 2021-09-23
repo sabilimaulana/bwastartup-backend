efee15a02b500abb37cb045a396c1cdafc232027
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-23 21:11:34 +0700

    Create Campaign API : Membuat Repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -10,6 +10,7 @@ type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
+	Save(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -49,6 +50,16 @@ func (r *repository) FindByID(ID int) (Campaign, error) {
 
 	if campaign.ID == 0 {
 		return campaign, errors.New("no campaign found on that id")
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) Save(campaign Campaign) (Campaign, error) {
+	err := r.db.Debug().Save(&campaign).Error
+
+	if err != nil {
+		return campaign, err
 	}
 
 	return campaign, nil
