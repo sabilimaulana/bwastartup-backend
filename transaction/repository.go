27e4b337ecfbab1b26c8ddc95d854ca4a7a33401
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-25 06:02:54 +0700

    User Transaction API : Membuat Repository

--- transaction/repository.go
+++ transaction/repository.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Repository interface {
 	GetByCampaignID(campaignID int) ([]Transaction, error)
+	GetByUserID(userID int) ([]Transaction, error)
 }
 
 type repository struct {
@@ -18,6 +19,17 @@ func (r *repository) GetByCampaignID(CampaignID int) ([]Transaction, error) {
 	var transactions []Transaction
 
 	err := r.db.Debug().Preload("User").Where("campaign_id = ?", CampaignID).Order("id desc").Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
+
+func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
+	var transactions []Transaction
+
+	err := r.db.Debug().Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Find(&transactions).Error
 	if err != nil {
 		return transactions, err
 	}
