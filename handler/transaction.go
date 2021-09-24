fb96eea41499be3a9511258415cef2489f4918ee
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-24 21:29:53 +0700

    Campaign Transaction API : JSON response formatter

--- handler/transaction.go
+++ handler/transaction.go
@@ -33,6 +33,6 @@ func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
 		return
 	}
 
-	response := helper.APIResponse("Successfully get campaign's transactions", http.StatusOK, "success", transactions)
+	response := helper.APIResponse("Successfully get campaign's transactions", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
 	c.JSON(http.StatusOK, response)
 }
--- transaction/formatter.go
+++ transaction/formatter.go
@@ -1,1 +1,42 @@
 package transaction
+
+import "time"
+
+type CampaignTransactionFormatter struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func FormatCampaignTransaction(transaction Transaction) CampaignTransactionFormatter {
+	formatterTransaction := CampaignTransactionFormatter{
+		ID:        transaction.ID,
+		Name:      transaction.User.Name,
+		Amount:    transaction.Amount,
+		CreatedAt: transaction.CreatedAt,
+	}
+
+	return formatterTransaction
+}
+
+func FormatCampaignTransactions(transactions []Transaction) []CampaignTransactionFormatter {
+	if len(transactions) == 0 {
+		return []CampaignTransactionFormatter{}
+	}
+
+	var formatterTransactions []CampaignTransactionFormatter
+
+	for _, transaction := range transactions {
+		formatterTransaction := CampaignTransactionFormatter{
+			ID:        transaction.ID,
+			Name:      transaction.User.Name,
+			Amount:    transaction.Amount,
+			CreatedAt: transaction.CreatedAt,
+		}
+
+		formatterTransactions = append(formatterTransactions, formatterTransaction)
+	}
+
+	return formatterTransactions
+}
