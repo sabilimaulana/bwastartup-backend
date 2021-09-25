66f0a9096cadb7f8ba6be779a7bebce43b687929
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-25 21:03:57 +0700

    User Transaction API : Membuat Service Midtrans bagian 1

--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
+	github.com/veritrans/go-midtrans v0.0.0-20210616100512-16326c5eeb00 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
---
+++ payment/service.go
@@ -0,0 +1,51 @@
+package payment
+
+import (
+	"bwastartup/transaction"
+	"bwastartup/user"
+	"log"
+	"strconv"
+
+	"github.com/veritrans/go-midtrans"
+)
+
+type Service interface {
+	GetToken(transaction transaction.Transaction, user user.User) (string, error)
+}
+
+type service struct {
+}
+
+func NewService() *service {
+	return &service{}
+}
+
+func (s *service) GetToken(transaction transaction.Transaction, user user.User) (string, error) {
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = "YOUR-VT-SERVER-KEY"
+	midclient.ClientKey = "YOUR-VT-CLIENT-KEY"
+	midclient.APIEnvType = midtrans.Sandbox
+
+	snapGateway := midtrans.SnapGateway{
+		Client: midclient,
+	}
+
+	snapReq := &midtrans.SnapReq{
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  strconv.Itoa(transaction.ID),
+			GrossAmt: int64(transaction.Amount),
+		},
+		CustomerDetail: &midtrans.CustDetail{
+			FName: user.Name,
+			Email: user.Email,
+		},
+	}
+
+	log.Println("GetToken:")
+	snapTokenResp, err := snapGateway.GetToken(snapReq)
+	if err != nil {
+		return "", err
+	}
+
+	return snapTokenResp.RedirectURL, nil
+}
