57b81f135b0e025d47623853a201369eb5dfa3e7
sabilimaulana <spacysbl.learn@gmail.com>
2021-10-04 21:06:26 +0700

    Web CMS Admin : Login Submit

--- main.go
+++ main.go
@@ -106,6 +106,7 @@ func main() {
 	router.GET("/transactions", authAdminMiddleware(), transactionWebHandler.Index)
 
 	router.GET("/login", sessionWebHandler.New)
+	router.POST("/session", sessionWebHandler.Create)
 
 	router.Run()
 }
